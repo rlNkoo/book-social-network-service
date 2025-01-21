d051146148e1a0c6c24067cd90f77625cf038feb
Artur Rolak <rolakartur96@gmail.com>
2025-01-21 18:11:58 +0100

    Project started

---
+++ src/main/java/com/rlnkoo/book/BookSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.rlnkoo.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSocialNetworkApplication.class, args);
+	}
+
+}
