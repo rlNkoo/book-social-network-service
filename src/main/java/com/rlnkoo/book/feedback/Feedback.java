9408c5f0fc1f6eebe1662f1c904850bd4a8c848a
Artur Rolak <rolakartur96@gmail.com>
2025-01-26 16:47:09 +0100

    Added Book, Feedback entities

---
+++ src/main/java/com/rlnkoo/book/feedback/Feedback.java
@@ -0,0 +1,22 @@
+package com.rlnkoo.book.feedback;
+
+import com.rlnkoo.book.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Feedback extends BaseEntity {
+
+    private Double note;
+
+    private String comment;
+}
